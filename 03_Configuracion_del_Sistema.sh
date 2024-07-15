1722afbd3f787509a88291fd9a0f4e8df2df03c0
suelectronico <suelectronico@gmail.com>
2024-07-15 19:50:23 +0200

    Update 03_Configuracion_del_Sistema.sh

--- 03_Configuracion_del_Sistema.sh
+++ 03_Configuracion_del_Sistema.sh
@@ -60,3 +60,64 @@ if [ \$? -ne 0 ]; then
 fi
 echo 'Configuración del teclado establecida correctamente.'
 
+read -p "Introduce el nombre del host que deseas configurar: " hostname
+if [ -z "$hostname" ]; then
+  echo "El nombre del host no puede estar vacío."
+  exit 1
+fi
+echo "Configurando el nombre del host en /etc/hostname..."
+echo "$hostname" > /etc/hostname
+if [ $? -ne 0 ]; then
+  echo "Error al configurar el nombre del host."
+  exit 1
+fi
+echo "/etc/hostname configurado correctamente."
+
+echo "Agregando entradas a /etc/hosts..."
+{
+  echo -e "127.0.0.1\tlocalhost"
+  echo -e "::1\tlocalhost"
+  echo -e "127.0.1.1\t${hostname}.localdomain ${hostname}"
+} >> /etc/hosts
+if [ $? -ne 0 ]; then
+  echo "Error al agregar entradas a /mnt/etc/hosts."
+  exit 1
+fi
+echo "/etc/hosts actualizado correctamente."
+
+read -s -p "Introduce la contraseña para el usuario root: " root_password
+echo 'Estableciendo la contraseña para el usuario root...'
+echo root:$root_password | chpasswd
+if [ \$? -ne 0 ]; then
+  echo 'Error al establecer la contraseña para el usuario root.'
+  exit 1
+fi
+echo 'Contraseña para el usuario root establecida correctamente.'
+
+read -p "Introduce el nombre del nuevo usuario: " new_user
+if [ -z "$new_user" ]; then
+  echo "El nombre del nuevo usuario no puede estar vacío."
+  exit 1
+fi
+echo 'Creando nuevo usuario y aplicando configuraciones...'
+useradd -m -g users -G wheel -s /bin/bash $new_user
+if [ \$? -ne 0 ]; then
+  echo 'Error al crear el nuevo usuario.'
+  exit 1
+fi
+read -s -p "Introduce la contraseña para el nuevo usuario: " new_user_password
+echo $new_user:$new_user_password | chpasswd
+if [ \$? -ne 0 ]; then
+  echo 'Error al establecer la contraseña para el nuevo usuario.'
+  exit 1
+fi
+echo 'Nuevo usuario creado y configurado correctamente.'
+
+echo 'Configurando sudoers para el nuevo usuario...'
+sed -i '/^root ALL=(ALL:ALL) ALL/a\\'$new_user' ALL=(ALL:ALL) ALL' /etc/sudoers
+if [ \$? -ne 0 ]; then
+  echo 'Error al configurar sudoers para el nuevo usuario.'
+  exit 1
+fi
+echo 'Configuración de sudoers para el nuevo usuario completada.'
+
